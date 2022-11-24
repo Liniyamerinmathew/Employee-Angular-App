2abb3a626e2e2fa57b87b0cc971421f3b69d5e58
Liniya Merin Mathew <Liniya@NTP-LAP-788.chn.nestgroup.net>
2022-11-24 15:59:28 +0530

    delete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DeleteEmployeeComponent } from './delete-employee/delete-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeleteEmployeeComponent
   ],
   imports: [
     BrowserModule,
