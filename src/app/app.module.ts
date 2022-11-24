b652c4457494d05ee273226aefd4ff5aa7b6094d
Liniya Merin Mathew <Liniya@NTP-LAP-788.chn.nestgroup.net>
2022-11-24 15:57:18 +0530

    search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchEmployeeComponent } from './search-employee/search-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchEmployeeComponent
   ],
   imports: [
     BrowserModule,
